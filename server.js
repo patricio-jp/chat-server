023f64e7b25293b8ed152be5f14f7aba973d86aa
patricio-jp <patojimenezpaz@gmail.com>
2025-05-10 22:25:23 -0300

    Eventos createChat y sendMessage agregados

--- server.js
+++ server.js
@@ -28,46 +28,134 @@ const onlineUsers = new Map(); // userId => Set(socketIds)
 io.use(socketAuthMiddleware);
 
 // Conexión por WebSocket
-io.on('connection', socket => {
-  console.log('🔌 Nuevo usuario conectado ', socket.user);
+io.on("connection", (socket) => {
+  console.log("🔌 Nuevo usuario conectado ", socket.user);
   const userId = socket.user.id;
 
-  // Cuando el usuario se conecta, lo agregás a la lista de online
+  // Importar los modelos
+  const User = require("./models/user.model");
+  const Chat = require("./models/chat.model");
+  const Message = require("./models/message.model");
+
+  // Cuando el usuario se conecta, agregarlo a la lista de usuarios online
   if (!onlineUsers.has(userId)) {
     onlineUsers.set(userId, new Set());
-    io.emit('userOnline', userId); // Notificás que este user se conectó
+    io.emit("userOnline", userId); // Notificar que dicho usuario se conectó
   }
   onlineUsers.get(userId).add(socket.id);
 
+  socket.on("createChat", async ({ isGroup, name, participants }) => {
+    //console.log({ isGroup, name, participants });
+    if (!Array.isArray(participants)) {
+      console.error(
+        "participants debe ser un arreglo de strings con ids de usuario"
+      );
+      return socket.emit("error:createChat", {
+        error: "participants debe ser un arreglo de strings con ids de usuario"
+      });
+    }
+    participants.push(socket.user.id); // Agregar al usuario que emitió el evento como participante
 
-  socket.on('sendMessage', ({ from, to, content }) => {
-    io.emit(`message:${to}`, { from, content }); // Reenvía al destinatario
-    // Acá podés guardar el mensaje en MongoDB también
+    isGroup = isGroup == true || participants.length > 2 ? true : false
+
+    // Verificar si ya existe un chat con los mismos participantes, solo para chats entre 2 personas
+    if (participants.length === 2) {
+      const existingChat = await Chat.findOne({
+        participants: { $all: participants, $size: participants.length },
+      });
+
+      if (existingChat) {
+        console.log("Ya existe un chat con los mismos participantes");
+        return socket.emit("error:createChat", {
+          error: "Ya existe un chat con los mismos participantes",
+          chatId: existingChat._id,
+        });
+      }
+    }
+    try {
+      const newChat = new Chat({
+        isGroup,
+        name: isGroup ? name : undefined, // Asignar nombre solo si es un grupo
+        participants,
+      });
+      await newChat.save();
+
+      participants.forEach(async (participant) => {
+        // Emitir el evento 'newChat' solo a los usuarios correspondientes
+        const userSockets = onlineUsers.get(participant);
+        if (userSockets) {
+          userSockets.forEach((socketId) => {
+            io.to(socketId).emit("newChat", newChat);
+          });
+        }
+
+        // Asignar el chat a cada participante
+        const user = await User.findById(participant);
+        if (user) {
+          user.chats.push(newChat);
+          await user.save();
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      io.emit("error:createChat", { error });
+    }
   });
 
+  socket.on("sendMessage", async ({ from, to, content }) => {
+    //console.log({ from, to, content });
+
+    if (!from) {
+      from = socket.user.id;
+    }
+
+    io.emit(`message:${to}`, { from, content }); // Reenvía al destinatario
+
+    // Guardado en la base de datos
+    try {
+      const chat = await Chat.findOne({
+        _id: to,
+      });
+      if (!chat) {
+        console.error("No existe el chat con el id proporcionado");
+        return io.emit("error:sendMessage", {
+          error: "No existe el chat con el id proporcionado",
+        });
+      }
+      const newMessage = new Message({
+        sender: from,
+        content,
+        chat: to,
+      });
+      await newMessage.save();
+    } catch (error) {
+      console.error(error);
+      return io.emit("error:sendMessage", { error });
+    }
+  });
 
   // Usuarios conectados
-  socket.on('whoIsOnline', async () => {
+  socket.on("whoIsOnline", async () => {
     // Supón que tienes un modelo User y cada usuario tiene un array de contactos (por ejemplo, user.contacts)
     const userId = socket.user.id;
     //const user = await User.findById(userId).select('contacts');
     //if (!user) return socket.emit('onlineUsers', []);
 
     //const online = user.contacts.filter(id => onlineUsers.has(id.toString()));
-    socket.emit('onlineUsers', online);
+    socket.emit("onlineUsers", online);
   });
 
-
   // Cuando se desconecta un usuario
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     const userSockets = onlineUsers.get(userId);
     if (!userSockets) return;
 
-    console.log('Usuario desconectado ', socket.user.id);
+    console.log("Usuario desconectado ", socket.user.id);
     userSockets.delete(socket.id);
     if (userSockets.size === 0) {
+      // Si el usuario no tiene mas sockets abiertos
       onlineUsers.delete(userId);
-      io.emit('userOffline', userId); // Notificás que este user se desconectó
+      io.emit("userOffline", userId); // Notificar que el usuario se desconectó
     }
   });
 });
